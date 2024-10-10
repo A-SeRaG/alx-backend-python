43b05d8e0372d739420dd205c826e5c1be0e71ae
SeRaG <aserag0101@gmail.com>
2024-10-10 21:00:55 +0300

    fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -7,7 +7,7 @@ This module provides a function `concat` to concatenated string
 def concat(str1: str, str2: str) -> str:
     """
     concatenated two strings.
-    
+
     Args:
         str1 (str): first string
         str2 (str): second string
