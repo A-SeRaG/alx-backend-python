49876d854e1ed4fc06000e9289ce1fb328ddb7a6
SeRaG <aserag0101@gmail.com>
2024-10-10 20:58:16 +0300

    str

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module provides a function `concat` to concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenated two strings.
+    
+    Args:
+    str1
+    """
+    return str1 + str2
