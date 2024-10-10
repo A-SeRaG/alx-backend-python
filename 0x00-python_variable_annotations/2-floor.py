6ed94bd2c8cb009b87b0341e0c832deca65c8486
SeRaG <aserag0101@gmail.com>
2024-10-10 21:08:54 +0300

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function `floor` to  returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+     Returns the floor of the float.
+
+    Args:
+        n (float): number
+
+    Returns:
+        int: floor of float
+    """
+    return n
