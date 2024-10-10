44df16dbcb20069de25198d78bf450e196f18e15
SeRaG <aserag0101@gmail.com>
2024-10-10 20:59:53 +0300

    str2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -9,6 +9,10 @@ def concat(str1: str, str2: str) -> str:
     concatenated two strings.
     
     Args:
-    str1
+        str1 (str): first string
+        str2 (str): second string
+
+    Returns:
+        str: concatenated string
     """
     return str1 + str2
